3c0e5fd9d65e1f88ffe374f64167b5436dd63f37
Mr-reinert <reinert.neto@gmail.com>
2025-11-27 22:43:15 -0300

    utils

--- src/utils/text_utils.py
+++ src/utils/text_utils.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import unidecode
 def normalize_text_columns(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Converte todas as colunas de texto (object/string) para maiúsculas
-    e remove acentos/caracteres especiais.
-
-    """
+    
+# Converte todas as colunas de texto (object/string) para maiúsculas e remove acentos/caracteres especiais.
     df_copy = df.copy()
     for col in df_copy.select_dtypes(include="object").columns:
         df_copy[col] = df_copy[col].astype(str).apply(
